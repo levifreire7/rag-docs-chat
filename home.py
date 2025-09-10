6ee0e0390ac9b9c426ed34a18a407ddfcb290aee
levifreire7 <levifreire7@gmail.com>
2025-09-09 23:54:02 -0300

    Cria sidebar para upload de arquivos

---
+++ home.py
@@ -0,0 +1,43 @@
+import streamlit as st
+from pathlib import Path
+import time
+
+PASTA_ARQUIVOS = Path(__file__).parent / "arquivos"
+
+
+def cria_chain_conversa():
+    st.session_state["chain"] = True
+    time.sleep(1)
+    pass
+
+
+def sidebar():
+    uploaded_pdfs = st.file_uploader(
+        "Adicione seus arquivos pdf", type=[".pdf"], accept_multiple_files=True
+    )
+    if not uploaded_pdfs is None:
+        for arquivo in PASTA_ARQUIVOS.glob("*.pdf"):
+            arquivo.unlink
+        for pdf in uploaded_pdfs:
+            with open(PASTA_ARQUIVOS / pdf.name, "wb") as f:
+                f.write(pdf.read())
+
+    label_botao = "Inicializar Chatbot"
+    if "chain" in st.session_state:
+        label_botao = "Atualizar Chatbot"
+    if st.button(label_botao, use_container_width=True):
+        if len(list(PASTA_ARQUIVOS.glob("*.pdf"))) == 0:
+            st.error("Adicione arquivos .pdf para inicializar o chatbot")
+        else:
+            st.success("Inicializando o Chatbot...")
+            cria_chain_conversa()
+            st.rerun()
+
+
+def main():
+    with st.sidebar:
+        sidebar()
+
+
+if __name__ == "__main__":
+    main()
